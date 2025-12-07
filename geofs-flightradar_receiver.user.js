74d034b8b6c632583a13da111ac5bd4709ba4b1e
seabus0316 <yongyuchao@gmail.com>
2025-12-07 22:25:58 +0800

    Update geofs-flightradar_receiver.user.js

--- geofs-flightradar_receiver.user.js
+++ geofs-flightradar_receiver.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         GeoFS ATC Radar
+// @name         GeoFS-flightradar receiver
 // @namespace    http://tampermonkey.net/
 // @version      1.9.4
 // @description  Always loads the latest GeoFS flightradar script from GitHub (I kust found that I can use this, thanks to xyzmani)
